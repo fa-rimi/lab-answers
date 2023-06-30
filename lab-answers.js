508a4905992c62a398bfcde7df394b5891df723e
rimi <a.fatimaarimi@gmail.com>
2023-06-30 12:49:03 -0400

    Fizz Buzz answered -- checkers almost solved

--- lab-answers.js
+++ lab-answers.js
@@ -8,3 +8,13 @@ for (let evenNum = 0; evenNum <= 200; evenNum+=2) {
     console.log(evenNum)
 }
 
+// FIZZ BUZZ === This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and shorter.
+for (let num = 1; num <= 100; num++) {
+    if (num % 3 === 0)
+    console.log('Fizz');
+    if (num % 5 === 0)
+    console.log('Buzz');
+    if (num % 3 === 0 && num % 5 === 0)
+    console.log("FizzBuzz")
+}
+
