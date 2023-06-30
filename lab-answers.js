70de8e2a5d6894043b88a1ffcc5c7dcd9d4ed66e
rimi <a.fatimaarimi@gmail.com>
2023-06-30 12:38:52 -0400

    Get Even answered

--- lab-answers.js
+++ lab-answers.js
@@ -3,4 +3,8 @@ for (let i = 1; i <= 20; i++) {
     console.log(i);
 }
 
+// GET EVEN === Write a for loop that will log only the even numbers in 0 through 200..
+for (let evenNum = 0; evenNum <= 200; evenNum+=2) {
+    console.log(evenNum)
+}
 
