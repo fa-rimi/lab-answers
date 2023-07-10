789123a1e3852785c15edfbcf190d7fa826501d1
rimi <a.fatimaarimi@gmail.com>
2023-07-10 17:28:13 -0400

    Find the Median answered

--- lab-answers.js
+++ lab-answers.js
@@ -162,3 +162,12 @@ for (let i = 0; i <= 20; i++) {
   }
 }
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// FIND THE MEDIAN === Find the median number in the following nums array, then console.log that number.
+const nums = [
+  14, 11, 16, 15, 13, 16, 15, 17, 19, 11, 12, 14, 19, 11, 15, 17, 11, 18, 12,
+  17, 12, 71, 18, 15, 12,
+];
+
+console.log(nums.sort()[Math.floor(nums.length / 2)]);
